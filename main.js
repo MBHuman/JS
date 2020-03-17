c366a268c52adea93870d580eb4e1e5ed8d411cd
MBHuman <zenka712@mail.ru>
2020-03-17 17:03:07 +0500

    Ничего такого не сделал

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-function solve(h, a ,b) {
-    return (h* (a + b));
+let pow = function(int x, int y) {
+    if(y == 0) {
+      return 1;
+    }
+    else if(y % 2 == 0) {
+      return pow(x, y / 2) * pow(x, y / 2);
+    }
+    else {
+      return pow(x, y - 1) * x;
+    }
+}
+
+let solve = function(int x) {
+  console.log(pow(x, 5));
 }
